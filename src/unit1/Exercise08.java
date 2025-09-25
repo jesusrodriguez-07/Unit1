4b7091c8e333838070c32e2e6880a525b73b195d
jesus.rodriguez <jesus.rodriguez@106-25.info2>
2025-09-25 13:07:22 +0200

    Commit

---
+++ src/unit1/Exercise08.java
@@ -0,0 +1,22 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Exercise08 {
+
+	public static void main(String[] args) {
+		int age;
+		boolean over18;
+		
+		Scanner scanner = new Scanner(System.in);
+		 System.out.println("Introduce your age: ");
+		 age = scanner.nextInt();
+		 
+		 over18 = age >= 18; 
+				
+		 System.out.println(over18);
+		 scanner.close();
+
+	}
+
+}
---
+++ src/unit1/Exercise10.java
@@ -0,0 +1,25 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+		// ask for a number and print if the number is even or odd
+		int number;
+		boolean even;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Write a number: ");
+		number = scanner.nextInt();
+		
+		even = number % 2 == 0;
+		
+		System.out.println("Your number is even?: " + even);
+		
+		
+		scanner.close();
+
+	}
+
+}
